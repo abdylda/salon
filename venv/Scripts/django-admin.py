fe77b6bc0d024ff3edc6c8c4089485dfc21e0fda
abdylda <93abdulla@mail.ru>
2021-06-21 15:56:49 +0600

    Lesson #1

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\user\Desktop\Diana\scalp-beauty-salon-website-master\scalp-beauty-salon-website-master\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
